64dccf50a65496f6d9fbe0386081a716d0342240
EdGinet <edwinginet@gmail.com>
2023-10-20 10:22:22 +0200

    modified database

--- migrations/Version20231020082057.php
+++ migrations/Version20231020082057.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231011100447 extends AbstractMigration
+final class Version20231020082057 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,21 +20,12 @@ final class Version20231011100447 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE comptes (id INT AUTO_INCREMENT NOT NULL, id_utilisateur INT NOT NULL, email VARCHAR(255) NOT NULL, mot_de_passe VARCHAR(255) NOT NULL, sel VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_56735801E7927C74 (email), UNIQUE INDEX UNIQ_56735801C6EE5C49 (id_utilisateur), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-
-        $this->addSql('CREATE TABLE utilisateurs (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, telephone VARCHAR(25) NOT NULL, entreprise VARCHAR(255) NOT NULL, poste VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-
-        $this->addSql('ALTER TABLE comptes ADD CONSTRAINT FK_56735801C6EE5C49 FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comptes DROP FOREIGN KEY FK_56735801C6EE5C49');
-        $this->addSql('DROP TABLE comptes');
-        $this->addSql('DROP TABLE utilisateurs');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
